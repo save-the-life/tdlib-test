e2ff593fdfca9e8e890eeba04fe1441a46313a66
LeeSunMyeong <lsmtjsaud@naver.com>
2024-07-19 16:33:28 +0900

    Initial commit

---
+++ src/main/java/com/slfoundation/tdlib_test/TdlibTestApplication.java
@@ -0,0 +1,13 @@
+package com.slfoundation.tdlib_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdlibTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdlibTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slfoundation/tdlib_test/TdlibTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slfoundation.tdlib_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdlibTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
